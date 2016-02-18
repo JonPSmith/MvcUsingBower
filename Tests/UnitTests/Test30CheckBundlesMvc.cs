922da95140a91a5d32672def47c713fc073cef73
Jon Smith <jon.smith@selectiveanalytics.com>
2016-02-18 15:01:01 +0000

    Rearranged the tests so that only MVC is checked normally as we cannot confirm the dates of the files in the Unit Test

---
+++ Tests/UnitTests/Test30CheckBundlesMvc.cs
@@ -0,0 +1,66 @@
+﻿#region licence
+// ======================================================================================
+// Mvc5UsingBower - An example+library to allow an MVC project to use Bower and Grunt
+// Filename: Test30CheckBundlesMvc.cs
+// Date Created: 2016/02/17
+// 
+// Under the MIT License (MIT)
+// 
+// Written by Jon Smith : GitHub JonPSmith, www.thereformedprogrammer.net
+// ======================================================================================
+#endregion
+
+using System.Linq;
+using B4BCore;
+using NUnit.Framework;
+using Tests.Helpers;
+using WebApplication.Mvc5;
+
+namespace Tests.UnitTests
+{
+    //This checks the MVC application's Bundles 
+    public class Test30CheckBundlesMvc
+    {
+
+        [Test]
+        public void TestCheckAllBundlesAreUpToDateInMvcAppOk()
+        {
+            //SETUP
+            var checker = new CheckBundles(typeof(BowerBundlerHelper));
+
+            //ATTEMPT
+            var errors = checker.CheckAllBundlesAreValid();
+
+            //VERIFY
+            errors.Any().ShouldEqual(false, string.Join("\n", errors));
+        }
+
+        [Test]
+        public void TestCheckBundleFileIsNotNewerThanMinifiedFilesMvcAppOk()
+        {
+            //SETUP
+            var checker = new CheckBundles(typeof(BowerBundlerHelper));
+
+            //ATTEMPT
+            var error = checker.CheckBundleFileIsNotNewerThanMinifiedFiles();
+
+            //VERIFY
+            error.ShouldEqual(null, error);
+        }
+
+        //------------------------------------------------------------
+        //Check with cdns (already checked by CheckAllBundlesAreValid, but done again to ensure there is no bugs in the checking code
+
+        [Test]
+        public void TestCheckSingleBundleIsUpToDateWithCdnOk()
+        {
+            var checker = new CheckBundles(typeof(BowerBundlerHelper));
+
+            //ATTEMPT
+            var errors = checker.CheckSingleBundleIsValid("standardLibsCndJs");
+
+            //VERIFY
+            errors.Any().ShouldEqual(false, string.Join("\n", errors));
+        }
+    }
+}
